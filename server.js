746d185efba0cc78bd8ecb375e272f4add0948d0
Colin Reinhardt <creinhardt@osano.com>
2025-09-10 14:03:10 -0400

    initial file structure

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
+
+// webhook endpoint
+
+app.post("/incoming-webhook", (req, res) => {
+  res.send("post");
+});
